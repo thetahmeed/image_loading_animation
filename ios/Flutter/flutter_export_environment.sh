3178c996de9e4082b498e5f2bdec8c223fab3c17
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2023-02-01 19:13:05 +0600

    IGNORE:

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Tahmeed\Desktop\my_flutter_packages\image_loading_animation"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Tahmeed\Desktop\GitHub\image_loading_animation"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Tahmeed\Desktop\my_flutter_packages\image_loading_animation"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Tahmeed\Desktop\GitHub\image_loading_animation"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
